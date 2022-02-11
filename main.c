df201b8bc0bbb98393539ed19f1e7b9c8d7bf81e
AmaliaR <amaliare@ucm.es>
2022-02-11 20:16:30 +0100

    Ejercicio 6.2.1 Entrada Programada

--- main.c
+++ main.c
@@ -10,6 +10,16 @@ extern void Eint4567_init(void);
 //extern void sys_init();
 /*--- declaracion de funciones ---*/
 //void Main(void);
+void buttons_init(){
+	int desp1 = 6*2; // Son seis pares de bits para el PIN6
+	int desp2 = 7*2; // Son siete pares de bits para el PIN7
+
+	rPCONG =~(0xf<<desp1); // Inicializamos los pines 6 y 7
+
+	rPUPG &= ~(0x1 <<6); //activamos las seniales de eint6
+	rPUPG &= ~(0x1 <<7); //activamos las seniales de eint7
+}
+
 /*--- codigo de funciones ---*/
 void Main(void)
 {
@@ -18,6 +28,8 @@ void Main(void)
 	/* Establecer valor inicial de los LEDs */
 	leds_off();
 	led1_on();
+
+	buttons_init();
 	while (1){
 		leds_switch();
 		DelayMs(1000);
